d31d7a5553b1333ec4df573806750905a34167d0
gozunbin <930083869@qq.com>
2025-07-30 17:04:12 +0800

    用户模块开发

---
+++ src/main/java/com/jun/junai/mapper/UserMapper.java
@@ -0,0 +1,13 @@
+package com.jun.junai.mapper;
+
+import com.mybatisflex.core.BaseMapper;
+import com.jun.junai.model.entity.User;
+
+/**
+ * 用户 映射层。
+ *
+ * @author 俊哥
+ */
+public interface UserMapper extends BaseMapper<User> {
+
+}
---
+++ src/main/java/com/jun/junai/service/UserService.java
@@ -0,0 +1,89 @@
+package com.jun.junai.service;
+
+import com.jun.junai.model.dto.user.UserQueryRequest;
+import com.jun.junai.model.vo.LoginUserVO;
+import com.jun.junai.model.vo.UserVO;
+import com.mybatisflex.core.query.QueryWrapper;
+import com.mybatisflex.core.service.IService;
+import com.jun.junai.model.entity.User;
+import jakarta.servlet.http.HttpServletRequest;
+
+import java.util.List;
+
+/**
+ * 用户 服务层。
+ *
+ * @author 俊哥
+ */
+public interface UserService extends IService<User> {
+    /**
+     * 用户注册
+     *
+     * @param userAccount   用户账户
+     * @param userPassword  用户密码
+     * @param checkPassword 校验密码
+     * @return 新用户 id
+     */
+    long userRegister(String userAccount, String userPassword, String checkPassword);
+
+    /**
+     * 获取加密密码
+     * @param userPassword 用户密码
+     * @return
+     */
+    String getEncryptPassword(String userPassword);
+
+    /**
+     * 获取脱敏的已登录用户信息
+     *
+     * @return
+     */
+    LoginUserVO getLoginUserVO(User user);
+
+    /**
+     * 用户登录
+     *
+     * @param userAccount  用户账户
+     * @param userPassword 用户密码
+     * @param request
+     * @return 脱敏后的用户信息
+     */
+    LoginUserVO userLogin(String userAccount, String userPassword, HttpServletRequest request);
+
+    /**
+     * 获取当前登录用户
+     *
+     * @param request
+     * @return
+     */
+    User getLoginUser(HttpServletRequest request);
+
+    /**
+     * 用户注销
+     *
+     * @param request
+     * @return
+     */
+    boolean userLogout(HttpServletRequest request);
+
+    /**
+     * 获取用户vo
+     * @param user
+     * @return
+     */
+    UserVO getUserVO(User user);
+
+    /**
+     * 获取用户vo列表
+     * @param userList
+     * @return
+     */
+    List<UserVO> getUserVOList(List<User> userList);
+
+    /**
+     * 查询包装
+     * @param userQueryRequest
+     * @return
+     */
+    QueryWrapper getQueryWrapper(UserQueryRequest userQueryRequest);
+}
